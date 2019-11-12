07b626a5de75a854d0900a37f12d81ee9001ceaa
도오성 <dos@dooseong-ui-MacBook-Pro.local>
2019-11-12 22:37:56 +0900

    초기화

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                메인
+            </div>
+        )
+    }
+}
+
+export default Main;
